63ef2bddf6301eff028e82ab8f2bb188e33bda48
jakobmajiddd <jakobmajid00@gmail.com>
2021-04-19 16:29:38 +0200

    tilføjet variabler til Order klassen

--- Marios-Pizzabar/src/Order.java
+++ Marios-Pizzabar/src/Order.java
@@ -1,2 +1,20 @@
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
 public class Order {
+  LocalDateTime dateTime;
+  double totalPrice;
+  ArrayList<Pizza> orderList = new ArrayList<>();
+
+  DateTimeFormatter formatDateTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    // Getter
+  public double getTotalPrice() {
+    return totalPrice;
+  }
+
 }
+
+
+
