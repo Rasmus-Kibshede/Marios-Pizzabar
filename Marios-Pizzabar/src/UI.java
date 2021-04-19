f1594fef717cbe2d2b516feef49c1cb027db59a2
Rasmus Kibshede <2740ralle@gmail.com>
2021-04-19 16:29:45 +0200

    edit in Main, Register and UI

--- Marios-Pizzabar/src/UI.java
+++ Marios-Pizzabar/src/UI.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class UI {
+
+  //Rasmus
+  public void printMenu(){
+
+  }
+
+  //Rasmus
+  public void printOrders(ArrayList<Order> orders){
+
+  }
+
+  //Rasmus
+  public int readMenuChoice(){
+    return 0;
+  }
+
+
+
 }
