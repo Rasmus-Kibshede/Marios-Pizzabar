e190572b2ff1409c353321d7e9ef149cd8693856
Alex-shadow1005 <kaareudlejningr@gmail.com>
2021-04-19 16:18:44 +0200

    PizzaClass opdateret kl 16.08 d.19-04

--- Marios-Pizzabar/src/Pizza.java
+++ Marios-Pizzabar/src/Pizza.java
@@ -1,2 +1,30 @@
 public class Pizza {
+    String name;
+    double price;
+    int number;
+
+    public void Pizza(String name, double price, int number){
+        this.name=name;
+        this.price=price;
+        this.number=number;
+
+    }
+    public int getNumber() {
+        return number;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public void setPrice(double price){
+        this.price=price;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
 }
