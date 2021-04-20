ee10c17e7683fcb55ab1029d191a9af075a601c1
Alex-shadow1005 <kaareudlejningr@gmail.com>
2021-04-20 12:04:42 +0200

    UI færdig (for nu) kl 12.04 d. 20/04

--- Marios-Pizzabar/src/UI.java
+++ Marios-Pizzabar/src/UI.java
@@ -2,32 +2,28 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UI {
-  Menu menu = new Menu();
+
   private Scanner scanner = new Scanner(System.in);
 
   //Rasmus + Alex
   public String printPizzaMenu(String messege){
     return messege;
   }
-  // Alex
+  //Alex
   public int printPizzaNumber(int pizzaNumber){
     return pizzaNumber;
   }
-  //Rasmus
-  public void printOrders(ArrayList<Order> orders){
-    //ArrayList here or in main ?
+  //Rasmus + Alex
+  public void printOrders(Order order){
+    System.out.println(order.getOrderList());
   }
 
   //Rasmus + Alex
-  // brug konstruktør fra menu
-  public int readMenuChoice(){
 
-    System.out.println("Seleckt one of the following opstions: ");
-    System.out.println("1. Show Menu");
-    System.out.println("2. Create Order");
-    System.out.println("3. Delete Order");
-    System.out.println("4. Statistics");
-    System.out.println("5. Quite");
+  public int printOptionsMenu(Menu menu){
+    System.out.println(menu.getMenuHeader());
+    System.out.println(menu.getMenuItems());
+    System.out.println(menu.getLeadText());
 
     return scanner.nextInt();
   }
