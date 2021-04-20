78603d11e816f7c14d957404ab8d6679259eb334
Alex-shadow1005 <kaareudlejningr@gmail.com>
2021-04-20 10:07:49 +0200

    Opdateret UI/readChoice kl10.07 d.20/4

--- Marios-Pizzabar/src/UI.java
+++ Marios-Pizzabar/src/UI.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UI {
 
@@ -14,7 +15,15 @@ public class UI {
 
   //Rasmus
   public int readMenuChoice(){
-    return 0;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Seleckt one of the following opstions: ");
+    System.out.println("1. Show Menu");
+    System.out.println("2. Create Order");
+    System.out.println("3. Delete Order");
+    System.out.println("4. Statistics");
+    System.out.println("5. Quite");
+
+    return scanner.nextInt();
   }
 
 
