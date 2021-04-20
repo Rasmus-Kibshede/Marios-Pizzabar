fa69fbe47b8b658e03ae28f93f73257aa94a1077
Alex-shadow1005 <kaareudlejningr@gmail.com>
2021-04-20 12:13:53 +0200

    opdatering af metode navne til brug i Register

--- Marios-Pizzabar/src/UI.java
+++ Marios-Pizzabar/src/UI.java
@@ -1,32 +1,34 @@
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class UI {
 
-  private Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
-  //Rasmus + Alex
-  public String printPizzaMenu(String messege){
-    return messege;
-  }
-  //Alex
-  public int printPizzaNumber(int pizzaNumber){
-    return pizzaNumber;
-  }
-  //Rasmus + Alex
-  public void printOrders(Order order){
-    System.out.println(order.getId());
-    System.out.println(order.getOrderList());
-    System.out.println(order.getTotalPrice());
-  }
+    //Rasmus + Alex
+    public String printString(String messege) {
+        return messege;
+    }
 
-  //Rasmus + Alex
+    //Alex
+    public int printInt(int pizzaNumber) {
+        return pizzaNumber;
+    }
 
-  public int printOptionsMenu(Menu menu){
-    System.out.println(menu.getMenuHeader());
-    System.out.println(menu.getMenuItems());
-    System.out.println(menu.getLeadText());
+    //Rasmus + Alex
+    public void printOrders(Order order) {
+        System.out.println(order.getId());
+        System.out.println(order.getOrderList());
+        System.out.println(order.getTotalPrice());
+    }
 
-    return scanner.nextInt();
-  }
+    //Rasmus + Alex
+
+    public int printOptionsMenu(Menu menu) {
+        System.out.println(menu.getMenuHeader());
+        System.out.println(menu.getMenuItems());
+        System.out.println(menu.getLeadText());
+
+        return scanner.nextInt();
+    }
 }
