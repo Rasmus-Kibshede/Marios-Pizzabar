ca261983ff11ed0d9e64dc346bb541d129d8d198
martinlaursen97 <martin_laursen21@hotmail.com>
2021-04-23 10:10:38 +0200

    Move statistics to its own class

--- Marios-Pizzabar/src/Register.java
+++ Marios-Pizzabar/src/Register.java
@@ -224,62 +224,6 @@ public class Register {
   }
 
   // Martin
-  public void showStatistics() {
-    ArrayList<String> storage = fileToList();
-    HashMap<String, Double> map = addToMap(storage);
-    iterateMap(map);
-  }
-
-  // Martin
-  public ArrayList<String> fileToList() {
-    ArrayList<String> storage = new ArrayList<>();
-
-    try {
-      Scanner input = new Scanner(new File("statistics.txt"));
-      while (input.hasNextLine()) {
-        String text = input.nextLine();
-        storage.add(text);
-      }
-      input.close();
-    } catch (FileNotFoundException e) {
-      ui.printColor("red", "File not found");
-    }
-    return storage;
-  }
-
-  // Martin
-  public HashMap<String, Double> addToMap(ArrayList<String> storage) {
-    HashMap<String, Double> statistic = new HashMap<>();
-
-    // Split every element in storage, then add to map. Key values get multiplied by the occurrences of the same key name
-    for (int i = 0; i < storage.size(); i++) {
-      String[] arr = storage.get(i).split("_");
-      String name = arr[0];
-      double price = Double.parseDouble(arr[1]);
-      statistic.put(name, price * Collections.frequency(storage, storage.get(i)));
-    }
-    return statistic;
-  }
-
-  // Martin
-  public void iterateMap(HashMap<String, Double> statistic) {
-    ArrayList<String> lst = new ArrayList<>();
-
-    // Iterates over the map, then adds it to lst
-    Iterator it = statistic.entrySet().iterator();
-    while (it.hasNext()) {
-      Map.Entry pair = (Map.Entry) it.next();
-      String s = "$" + pair.getValue() + " \t " + pair.getKey();
-      lst.add(s);
-      it.remove();
-    }
-
-    for (int i = 0; i < lst.size(); i++) {
-      ui.printString((i + 1) + ": " + lst.get(i));
-    }
-  }
-
-  // Martin
   public void clearOrders() {
     ui.printColor("yellow", "Are you sure you want to clear the orders? \"y\" to clear.");
     ui.printStringAppend("Choice: ");
@@ -288,6 +232,13 @@ public class Register {
       orders.clear();
       saveOrder();
     }
+  }
+
+  public void showStatistics() {
+    Statistics stats = new Statistics();
+    ArrayList<String> storage = stats.fileToList();
+    HashMap<String, Double> map = stats.addToMap(storage);
+    stats.iterateMap(map);
   }
 
   public int validateChoice(String text) {
---
+++ Marios-Pizzabar/src/Statistics.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Statistics {
+    UI ui = new UI();
+
+    // Martin
+    public ArrayList<String> fileToList() {
+        ArrayList<String> storage = new ArrayList<>();
+
+        try {
+            Scanner input = new Scanner(new File("statistics.txt"));
+            while (input.hasNextLine()) {
+                String text = input.nextLine();
+                storage.add(text);
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            ui.printColor("red", "File not found");
+        }
+        return storage;
+    }
+
+    // Martin
+    public HashMap<String, Double> addToMap(ArrayList<String> storage) {
+        HashMap<String, Double> statistic = new HashMap<>();
+
+        // Split every element in storage, then add to map. Key values get multiplied by the occurrences of the same key name
+        for (int i = 0; i < storage.size(); i++) {
+            String[] arr = storage.get(i).split("_");
+            String name = arr[0];
+            double price = Double.parseDouble(arr[1]);
+            statistic.put(name, price * Collections.frequency(storage, storage.get(i)));
+        }
+        return statistic;
+    }
+
+    // Martin
+    public void iterateMap(HashMap<String, Double> statistic) {
+        ArrayList<String> lst = new ArrayList<>();
+
+        // Iterates over the map, then adds it to lst
+        Iterator it = statistic.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            String s = "$" + pair.getValue() + " \t " + pair.getKey();
+            lst.add(s);
+            it.remove();
+        }
+
+        for (int i = 0; i < lst.size(); i++) {
+            ui.printString((i + 1) + ": " + lst.get(i));
+        }
+    }
+}
